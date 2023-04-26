cd40cf2474138145a194191c2462a89be1e7705a
Rameez <raheview@gmail.com>
2023-04-26 15:19:44 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="RHD">
+      <div className="RH-banner bg-primary">
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
